2ff0494c9521db148d3b469aea8dd7e85fbfb25c
Mayro <deehmyller11@gmail.com>
2021-04-20 01:13:28 -0300

    chore: add font family tiktok

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: 'sofiapro';
+    src: url('../../public/fonts/sofiapro.ttf') format('trutype');
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html, body{
+    font-family: sofipro, sans-serif;
   }
 `;
 
